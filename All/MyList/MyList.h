8dd1462b9b24788b795b9be4bc065503ccbfc612
ko1o6ok <woolfer707@gmail.com>
2022-12-20 19:16:06 +0300

    Trying to sort pointers

--- All/MyList/MyList.h
+++ All/MyList/MyList.h
@@ -5,7 +5,7 @@ template <typename T>
 struct Node {
     T value;
     Node<T>* nxt;
-    explicit Node(T _v){
+    explicit Node(T _v) {
         value = _v;
         nxt = nullptr;
     }
@@ -15,7 +15,7 @@ class MyList {
 public:
     Node<T>* first;
     Node<T>* last;
-    MyList(){
+    MyList() {
         first = nullptr;
         last = nullptr;
     }
@@ -66,16 +66,16 @@ public:
     }
     void insert(T val, const int pos) {
         auto* new_node = new Node<T>(val);
-        Node<T>* s1 = this->operator[](pos-1);
+        Node<T>* s1 = this->operator[](pos - 1);
         Node<T>* s2 = s1->nxt;
         s1->nxt = new_node;
         new_node->nxt = s2;
     }
-    void reverse(){
+    void reverse() {
         // Длина должна быть >= 3 , иначе можно сразу переставить
-        if (empty()||(first == last)) return; // Длина = 0 или 1, ничего делать не надо
+        if (empty() || (first == last)) return; // Длина = 0 или 1, ничего делать не надо
         Node<T>* b = first->nxt;
-        if(b == last){ // Длина = 2, можно элементарно переставить: a -> b => b -> a
+        if (b == last) { // Длина = 2, можно элементарно переставить: a -> b => b -> a
             first->nxt = nullptr;
             last->nxt = first;
             Node<T>* temp;
@@ -90,8 +90,8 @@ public:
         // <- -> -> -> -> перейдёт в <- <- -> -> -> , что далее перейдёт в <- <- <- -> ->
         Node<T>* a = first;
         Node<T>* c = b->nxt;
-        a->nxt= nullptr;
-        while (c != last){
+        a->nxt = nullptr;
+        while (c != last) {
             // Здесь происходит сдвиг узла
             b->nxt = a;
             a = b;
@@ -115,11 +115,132 @@ public:
         }
         Node<T>* t = l.first;
         while (t) {
-            ostr << t->value <<" ";
+            ostr << t->value << "->";
             t = t->nxt;
         }
         ostr << endl;
         return ostr;
     }
+    // Есть ли в этом списке цикл
+    bool hasCycle() {
+        // Лишь один элемент
+        if (first == last)
+            return false;
+        // Два элемента
+        if (first->nxt == last)
+            return first == last->nxt;
+
+        Node<T>* tortoise = first;
+        Node<T>* hare = tortoise->nxt;
+        
+        //int tort_count = 1;
+        //int hare_count = 2;
+        while (tortoise != hare) {
+            if (hare->nxt->nxt == nullptr)
+                return false;
+            tortoise = tortoise->nxt;
+            //tort_count++;
+            hare = hare->nxt->nxt;
+            //hare_count += 2;
+            //cout << "Tortoise: " << tort_count << "| Hare: " << hare_count << endl;
+        }
+        return true;
+    }
+
+    Node<T>* find_middle() {
+        // Начинают с одной позиции
+        Node<T>* tortoise = first;
+        Node<T>* hare = first;
+        // Если элемент только один, то он же и является серединой
+        if (first == last)
+            return first;
+        // Для чётного числа элементов серединой будем считать левый из 2-х средних элементов
+        // Это связано с тем, что заец в два раза быстрее черепахи и придёт именно туда
+        while (hare->nxt->nxt != nullptr)
+        {
+            tortoise = tortoise->nxt;
+            hare = hare->nxt->nxt;
+            if (hare->nxt == nullptr)
+                break;
+        }
+        return tortoise;
+    }
+    void perform_bubblesort() {
+        // Только один элемент
+        if (first == last)
+            return;
+        // Два элемента
+        if (first->nxt == last) {
+            if (first->value > last->value) {
+                Node<T>* temp;
+                temp = first;
+                first = last;
+                first->nxt = temp;
+                last = temp;
+                last->nxt = nullptr;
+            }
+            return;
+        }
+        // Теперь >= 3 элемента
+        // Я иду тройками, чтобы устанавливать nxt предыд. элемента
+        Node<T>* one;
+        Node<T>* two;
+        Node<T>* three;
+        
+        bool swapped;
+        while (true)
+        {
+            swapped = false;
+            one = first;
+            two = one->nxt;
+            if (two == nullptr)
+                break;
+            three = two->nxt;
+            // Но всё равно нужно обработать первые 2 элемента
+            if (one->value > two->value) {
+                Node<T>* temp;
+                temp = one;
+                one = two;
+                one->nxt = temp;
+                two = temp;
+                one->nxt = three;
+                swapped = true;
+            }
+            
+            //cout << *this << endl;
+            while (three->nxt != nullptr) {
+                cout << three->nxt << endl;
+                if (two->value > three->value) {
+                    one->nxt = three;
+                    cout << one << ", " << two << ", " << three << endl;
+                    swap_nodes(two, three);
+                    cout << one->nxt << ", " << two->nxt << ", " << three->nxt << endl;
+                    swapped = true;
+                    one->nxt = three;
+                    //cout << *this << endl;
+                    one = three;
+                    three = two->nxt;
+                    
+                }
+                else {
+                    one = one->nxt;
+                    two = two->nxt;
+                    three = three->nxt;
+                }
+                
+            }
+            
+            if (swapped == false)
+                break;
+        }
+    }
 };
 
+template <typename T>
+void swap_nodes(Node<T>* n2, Node<T>* n3) {
+    Node<T>* t1 = n3->nxt;
+    Node<T>*  t2 = n2;
+    n2 = n3;
+    n2->nxt = t1;
+    n3 = t2;
+}
